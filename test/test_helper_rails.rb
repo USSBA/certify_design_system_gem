a3d91f8a4301cfd203b9351388f2aaa8ca7dd156
rebel-one <david_m_acevedo@omb.eop.gov>
2018-03-20 17:25:45 -0400

    todo: do gray scale image

--- test/test_helper_rails.rb
+++ test/test_helper_rails.rb
@@ -30,20 +30,6 @@ end
 
 Capybara.javascript_driver = :headless_chrome
 
-# Capybara.register_driver :firefox_headless do |app|
-#   options = ::Selenium::WebDriver::Firefox::Options.new
-#   options.args << '--headless'
-#   options.args << '--marionette'
-
-#   Capybara::Selenium::Driver.new(app,
-#                                  browser: :firefox,
-#                                  marionette: true,
-#                                  options: options)
-# end
-# Selenium::WebDriver::Firefox::Binary.path =
-#   '/Applications/FirefoxDeveloperEdition.app/Contents/MacOS/firefox'
-# Capybara.javascript_driver = :firefox_headless
-
 Capybara.configure do |config|
   config.app_host              = 'http://localhost:7000'
   config.default_driver        = :headless_chrome
@@ -51,14 +37,6 @@ Capybara.configure do |config|
   config.server_port           = 7000
   config.default_max_wait_time = 10
 end
-
-# Capybara.configure do |config|
-#   config.app_host              = 'http://localhost:7000'
-#   config.default_driver        = :firefox_headless
-#   config.javascript_driver     = :firefox_headless
-#   config.server_port           = 7000
-#   config.default_max_wait_time = 10
-# end
 
 def suppress_output
   original_stdout = $stdout.clone
