10966e21b8b4fc4139b8c2fdfdd2b1d2941e41cf
tbolt <tybolt@gmail.com>
2018-04-26 11:15:09 -0400

    Doc upload draft (#12)
    * doc upload initial wip
    
    * wip doc-upload example
    
    * wip doc upload interactions
    
    * updates to doc component
    
    * refactor doc-upload component
    
    * change border on file input
    
    * update version, cleanup css

---
+++ app/assets/javascripts/components/doc-upload.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  // Variables
+  var $doc_upload_toggle = $('.sba-c-doc-upload__toggle'),
+      hidden_class = "hidden",
+      open_class = "is-open",
+      visible_class = "is-visible";
+
+  // Function for handling doc upload
+  $doc_upload_toggle.on('click', function() {
+
+    // Determine which task panel we clicked on
+    elem = $(this).attr("aria-controls")
+    var $toggle = $('button[aria-controls=' + elem + ']');
+    var $cancel = $('button[aria-controls=' + elem + '-cancel]');
+    var $target = $("#" + elem);
+
+    // Toggle doc uploader
+    if ($toggle.attr("aria-expanded") === "false") {
+      $toggle.attr("aria-expanded", "true");
+      $toggle.addClass(hidden_class);
+      $target.parent().addClass(open_class);
+      $target.addClass(visible_class);
+    }
+
+    // Toggle cancel
+    $cancel.on('click', function() {
+      $toggle.attr("aria-expanded", "false");
+      $toggle.removeClass(hidden_class);
+      $target.parent().removeClass(open_class);
+      $target.removeClass(visible_class);
+    })
+  });
+});
