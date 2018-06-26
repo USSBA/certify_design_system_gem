05141db1dcb0e2adbfe83df490ffb249a4d300f0
Jared C Cunha <jcunha@omb.eop.gov>
2018-06-26 12:22:52 -0400

    add tooltip component (#24)

--- lib/certify_design_system/version.rb
+++ lib/certify_design_system/version.rb
@@ -6,5 +6,5 @@ module CertifyDesignSystem
   #    MAJOR version when you make incompatible API changes,
   #    MINOR version when you add functionality in a backwards-compatible manner, and
   #    PATCH version when you make backwards-compatible bug fixes.
-  VERSION = '1.11.1'
+  VERSION = '1.12.0'
 end
