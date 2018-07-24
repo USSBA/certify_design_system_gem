28920d72a6eb2ff996c87ad431816286a6d3a55f
Jared Cunha <jcunha@omb.eop.gov>
2018-07-24 12:30:13 -0400

    add font-weight utility

--- lib/certify_design_system/version.rb
+++ lib/certify_design_system/version.rb
@@ -6,5 +6,5 @@ module CertifyDesignSystem
   #    MAJOR version when you make incompatible API changes,
   #    MINOR version when you add functionality in a backwards-compatible manner, and
   #    PATCH version when you make backwards-compatible bug fixes.
-  VERSION = '1.13.4'
+  VERSION = '1.14.0'
 end
