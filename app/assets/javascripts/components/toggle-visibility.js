5fa76096f3bf228323cb251e4e4d531da734b093
Jared Cunha <jcunha@omb.eop.gov>
2018-08-31 13:00:21 -0400

    add new topnav section

--- app/assets/javascripts/components/toggle-visibility.js
+++ app/assets/javascripts/components/toggle-visibility.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
-  var $trigger = $('.sba-js-visibility-trigger');
+  var $trigger = $('.sba-js-visibility-trigger'),
+      visible_class = "is-visible";
 
   $trigger.on('click', function(){
     var target = "#" + $(this).attr('aria-controls');
     if ($(this).attr("aria-expanded") === "false" ) {
-      $(this).attr("aria-expanded", "true").css('transform', 'rotate(180deg)');
-      $(target).removeAttr("hidden");
+      $(this).attr("aria-expanded", "true");
+      $(target).removeAttr("hidden").addClass(visible_class);
     }
     else {
-      $(this).attr("aria-expanded", "false").removeAttr("style");
-      $(target).attr("hidden", "");
+      $(this).attr("aria-expanded", "false");
+      $(target).attr("hidden", "").removeClass(visible_class);
     }
     return false;
   });
